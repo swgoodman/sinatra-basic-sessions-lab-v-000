9afcc36a9e2beff9a5cbd932b05cac197ddad93b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 21:41:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    session[:item] = params[:item]
+    erb :checkout
+  end
 end
